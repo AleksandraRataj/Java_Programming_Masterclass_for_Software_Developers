85f199bc98d7c376024ff3d8eeb35e6bea9ec208
AleksandraRataj <aleksandrarataj2@gmail.com>
2020-02-19 21:59:00 +0100

    Dodano kolejne ćwiczenia

---
+++ CodingExercises/src/EqualSumChecker.java
@@ -0,0 +1,14 @@
+public class EqualSumChecker {
+
+    public static void main(String[] args) {
+
+        System.out.println(hasEqualSum(1,1,2));
+        System.out.println(hasEqualSum(1,2,4));
+    }
+
+    public static boolean hasEqualSum(int firstNumber, int secondNumber, int sum){
+
+        return(firstNumber + secondNumber) == sum;
+    }
+
+}
