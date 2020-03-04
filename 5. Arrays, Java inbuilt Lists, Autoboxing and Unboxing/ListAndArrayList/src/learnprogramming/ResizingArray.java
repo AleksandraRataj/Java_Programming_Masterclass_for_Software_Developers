a4a65a3e7c2676cf6b54bbacbc467f94f7e9bb42
AleksandraRataj <aleksandrarataj2@gmail.com>
2020-03-04 12:10:03 +0100

    Arrays List - exercises added.

---
+++ 5. Arrays, Java inbuilt Lists, Autoboxing and Unboxing/ListAndArrayList/src/learnprogramming/ResizingArray.java
@@ -0,0 +1,46 @@
+package learnprogramming;
+
+import java.util.Scanner;
+
+public class ResizingArray {
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static int[] baseData = new int[10];
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter 10 integers: ");
+        getInput();
+        printArray(baseData);
+        resizeArray();
+//        System.out.println("Enter 12 integers: ");
+//        getInput();
+        baseData[10] = 7;
+        baseData[11] = 99;
+        printArray(baseData);
+
+    }
+
+    private static void getInput() {
+        for (int i = 0; i < baseData.length; i++) {
+            baseData[i] = scanner.nextInt();
+        }
+    }
+
+    private static void printArray(int[] array) {
+        for (int i = 0; i < baseData.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    private static void resizeArray() {
+
+        int[] original = baseData;
+        baseData = new int[12];
+        for (int i = 0; i < original.length; i++) {
+            baseData[i] = original[i];
+        }
+    }
+
+}
