2b91b46e5c7a58e9adb3f33775d99e45d7efd595
AleksandraRataj <aleksandrarataj2@gmail.com>
2020-05-04 11:16:04 +0200

    Added JavaFXApplication.

---
+++ 10. JavaFX/JavaFXApplication/src/module-info.java
@@ -0,0 +1,10 @@
+module JavaFXApplication {
+
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires jlfgr;
+    requires java.desktop;
+    requires javafx.web;
+
+    opens sample;
+}
